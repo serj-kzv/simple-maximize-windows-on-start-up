0731afd1ff6d6d208b6017f27809f43fd39ce8fe
serj-kzv <serj.kzv@gmail.com>
2019-07-07 12:18:07 +0300

    forth commit, release

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ const maximizeAll = async () => {
             ...(await windowsApi.getAll())
                 .map(async ({id}) => windowsApi.update(id, maximized)),
             async () =>
-                // Brings current Firefox's window to the front after all windows are maximized.
+                // Brings a current Firefox's window to the front after all windows are maximized.
                 // It's because the current window can be folded and maximized at the same time.
                 windowsApi.update((await windowsApi.getCurrent()).id, {focused: true})
         ]
