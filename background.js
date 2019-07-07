f99c2fad4ed21f12327878a51174837491a29206
null <null>
2019-07-07 12:11:41 +0300

    second commit

---
+++ background.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const maximizeAll = async () => {
+    const windowsApi = browser.windows, maximized = {state: 'maximized'};
+
+    Promise.all(
+        [
+            ...(await windowsApi.getAll())
+                .map(async ({id}) => windowsApi.update(id, maximized)),
+            async () =>
+                // Brings current Firefox's window to the front after all windows are maximized.
+                // It's because the current window can be folded and maximized at the same time.
+                windowsApi.update((await windowsApi.getCurrent()).id, {focused: true})
+        ]
+    );
+};
+
+browser.runtime.onStartup.addListener(maximizeAll);
+browser.browserAction.onClicked.addListener(maximizeAll);
